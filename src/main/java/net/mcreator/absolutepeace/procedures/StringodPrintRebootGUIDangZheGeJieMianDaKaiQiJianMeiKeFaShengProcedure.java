639f9d5d8a4c06e6791978c5c4d72dd4400c01dd
m13619199464@163.com <m13619199464@163.com>
2022-10-01 18:34:45 +0800

    调整了打印机打印物品的数量

--- src/main/java/net/mcreator/absolutepeace/procedures/StringodPrintRebootGUIDangZheGeJieMianDaKaiQiJianMeiKeFaShengProcedure.java
+++ src/main/java/net/mcreator/absolutepeace/procedures/StringodPrintRebootGUIDangZheGeJieMianDaKaiQiJianMeiKeFaShengProcedure.java
@@ -3,19 +3,22 @@ package net.mcreator.absolutepeace.procedures;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.inventory.Slot;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.util.Mth;
 import net.minecraft.server.level.ServerPlayer;
 
 import java.util.function.Supplier;
+import java.util.Random;
 import java.util.Map;
 
 public class StringodPrintRebootGUIDangZheGeJieMianDaKaiQiJianMeiKeFaShengProcedure {
 	public static void execute(Entity entity) {
 		if (entity == null)
 			return;
+		double num = 0;
 		if (entity instanceof ServerPlayer _player && _player.containerMenu instanceof Supplier _current && _current.get() instanceof Map _slots) {
 			ItemStack _setstack = (entity instanceof ServerPlayer _plrSlotItem && _plrSlotItem.containerMenu instanceof Supplier _splr
 					&& _splr.get() instanceof Map _slt ? ((Slot) _slt.get(0)).getItem() : ItemStack.EMPTY);
-			_setstack.setCount(1);
+			_setstack.setCount(Mth.nextInt(new Random(), 1, 64));
 			((Slot) _slots.get(1)).set(_setstack);
 			_player.containerMenu.broadcastChanges();
 		}
